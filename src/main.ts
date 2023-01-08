3769892650eb6c1984f30ab197f6afd017407de1
Scott Onson <onsons@ssoworld.com>
2023-01-08 12:58:22 -0600

    chore: set up script and start test

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,61 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {execSync} from 'child_process'
+import path from 'path'
+import fs from 'fs'
+import {assertCoverageThreshold} from './coverage'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const output: string = core.getInput('output')
+    const outputFormat: string = core.getInput('outputFormat')
+    let settingsFile: string = core.getInput('settingsFile')
+    let thresholdstring: string = core.getInput('threshold')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const rootpath = path.dirname('./')
+    const coverageFile = `${rootpath}/${output}`
+    const runSettingsFile = `${rootpath}/${settingsFile}`
 
-    core.setOutput('time', new Date().toTimeString())
+    if (!fs.existsSync(coverageFile)) {
+      core.setFailed(
+        `error occurred : runsettings file not found at ${settingsFile}`
+      )
+    }
+
+    /****************************************/
+    /****                                ****/
+    /****  create coverlet args          ****/
+    /****                                ****/
+    /****************************************/
+
+    const properties: string = `--collect:"XPlat Code Coverage" --settings coverlet.runsettings /p:CollectCoverage=true /p:CoverletOutputFormat=${outputFormat}`
+    // let properties: string = `-p:coverletOutput=${output} -p:CollectCoverage=true -p:CoverletOutputFormat=${outputFormat}`;
+
+    const execString: string = `run dotnet test -c Debug ${properties}`
+    console.log(execString)
+
+    /* ***************************************/
+    /* ***                                ****/
+    /* ***  run dotnet test               ****/
+    /* ***                                ****/
+    /* ***************************************/
+
+    try {
+      const dotnet: any = execSync(execString)
+      console.log(`dotnet succeeded`)
+      assertCoverageThreshold(dotnet, thresholdstring)
+    } catch (error) {
+      console.log(`dotnet failed`)
+      if (error instanceof Error) {
+        assertCoverageThreshold(error.message, thresholdstring)
+        core.setFailed(`dotnet test failure ${error.message}`)
+      }
+    }
+
+    if (!fs.existsSync(coverageFile)) {
+      core.setFailed(
+        `error occurred : coverage file not found at ${coverageFile}`
+      )
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
